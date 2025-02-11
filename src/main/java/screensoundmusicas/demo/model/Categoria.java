945d045b184e8b06766277586dd8ba882fb81a1c
WillianMoreiraBFP <162322947+WillianMoreiraBFP@users.noreply.github.com>
2025-02-11 12:40:14 -0300

    EnunCategoria

--- src/main/java/screensoundmusicas/demo/model/Categoria.java
+++ src/main/java/screensoundmusicas/demo/model/Categoria.java
@@ -1,4 +1,20 @@
 package screensoundmusicas.demo.model;
 
 public enum Categoria {
+    SOLO("Solo"),
+    DUPLA("Dupla"),
+    BANDA("Banda");
+
+    private String categoria;
+
+    Categoria(String categoria){this.categoria = categoria;}
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoria.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
 }
